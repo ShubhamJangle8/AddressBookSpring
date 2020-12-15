f02bdb5c2efba283f41c5618f50cd7c58426ac17
jangle <shubhamjangale0000@gmail.com>
2020-12-15 16:57:27 +0530

    Added : Added the logging functions using Lombok

--- src/main/java/com/capge/demo/AddressBookSpringApplication.java
+++ src/main/java/com/capge/demo/AddressBookSpringApplication.java
@@ -2,12 +2,18 @@ package com.capge.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSpringApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookSpringApplication.class, args);
+		log.info("AddressBook app App started in {} environment", context.getEnvironment().getProperty("environment"));
+		log.info("Address book DB user is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
