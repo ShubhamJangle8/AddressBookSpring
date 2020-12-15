408e4322ef050d204056ac5a3ada9e71fb6af9a7
jangle <shubhamjangale0000@gmail.com>
2020-12-15 16:50:58 +0530

    Modified : Modified code to use Lambok for generating getters and setters

--- src/main/java/com/capge/demo/dto/PersonDTO.java
+++ src/main/java/com/capge/demo/dto/PersonDTO.java
@@ -1,6 +1,8 @@
 package com.capge.demo.dto;
 
-public class PersonDTO {
+import lombok.Data;
+
+public @Data class PersonDTO {
 	
 	private String firstName;
 	private String lastName;
@@ -10,51 +12,4 @@ public class PersonDTO {
 	private String zipCode;
 	private String phoneNumber;
 	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getZipCode() {
-		return zipCode;
-	}
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	@Override
-	public String toString() {
-		return "PersonDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
-				+ city + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + "]";
-	}
 }
--- src/main/java/com/capge/demo/model/PersonData.java
+++ src/main/java/com/capge/demo/model/PersonData.java
@@ -2,7 +2,9 @@ package com.capge.demo.model;
 
 import com.capge.demo.dto.PersonDTO;
 
-public class PersonData {
+import lombok.Data;
+
+public @Data class PersonData {
 	
 	private Integer id;
 	private String firstName;
@@ -24,67 +26,4 @@ public class PersonData {
 		this.phoneNumber = personDTO.getPhoneNumber();
 	}
 	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
 }
